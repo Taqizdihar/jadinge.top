cd5b930cb78fc98b07a26950a6e033d2d959ae9f
Taqizdihar <m.taqizdihar@gmail.com>
2025-12-04 20:45:26 +0700

    Hold on, guys. Database is constructing and on progress, trying both MongoDB and MySQL.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,15 +6,10 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
+    public function run(): void {
         $this->call(UserSeeder::class);
     }
 }
