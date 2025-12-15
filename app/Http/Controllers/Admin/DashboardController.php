8c4e4c0323df9aa363c8cc1d29562a96d700d736
Nanda <nandacandikia639@gmail.com>
2025-12-15 07:51:01 +0700

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,1 +1,16 @@
- 
+<?php
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Ads;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $activeAds = Ads::where('status', 'active')->count();
+        $locations = 5; // contoh dummy data
+
+        return view('admin.dashboard', compact('activeAds', 'locations'));
+    }
+}
